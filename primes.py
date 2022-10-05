53106195524dac6338f75f587fd4469297923edd
Jaeho Lee <jaeho.lee@kcl.ac.uk>
2022-10-05 18:35:08 +0100

    Return a list of prime numbers

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+
+    lower = 2
+    upper = number_of_primes
+
+    for num in range(lower, upper + 1):
+        for i in range(2, num):
+            if num % i == 0:
+                break
+            else:
+                list.append(num)
+                
+
     return list
