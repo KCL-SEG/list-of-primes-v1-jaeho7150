00130a072a48240ff36d58370c47f7ccfc2bd6ac
Jaeho Lee <jaeho.lee@kcl.ac.uk>
2022-10-05 19:35:16 +0100

    new change

--- primes.py
+++ primes.py
@@ -4,7 +4,7 @@
 def primes(number_of_primes):
     list = []
     curr_num = 1
-    while len(list) <= number_of_primes:
+    while len(list) < number_of_primes:
         if curr_num > 1:
             for i in range(2, curr_num):
                 if curr_num % i == 0:
